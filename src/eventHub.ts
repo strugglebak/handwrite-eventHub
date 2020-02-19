3d6be19c8ce1ecd3c59baaeec4fe01b9c2b900b3
moss <dxp157@sohu.com>
2020-02-19 10:28:56 +0800

    代码重构

--- src/eventHub.ts
+++ src/eventHub.ts
@@ -2,16 +2,13 @@ class EventHub {
   // 监听/注册事件
   cache = {}
   on(eventName, fn) {
-    let array = this.cache[eventName]
-    !array && (array = [])
-    array.push(fn) // 监听注册过的事件
+    this.cache[eventName] = this.cache[eventName] || []
+    this.cache[eventName].push(fn) // 监听注册过的事件
   }
 
   // 触发事件
   emit(eventName) {
-    let array = this.cache[eventName]
-    !array && (array = [])
-    array.forEach(fn => fn()) // 执行注册过的事件
+    (this.cache[eventName] || []).forEach(fn => fn()) // 执行注册过的事件
   }
 }
 
