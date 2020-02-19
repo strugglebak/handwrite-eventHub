12768e63dc938d5370588b7f7c7ffe303d201d7c
moss <dxp157@sohu.com>
2020-02-19 12:33:52 +0800

    加上 typescript 断言

--- src/eventHub.ts
+++ src/eventHub.ts
@@ -1,7 +1,12 @@
 class EventHub {
   // 监听/注册事件
-  cache = {}
-  private indexOf(array, item) {
+  private cache: {
+    [key: string]: Array<(data?: unknown) => void>
+  } = {}
+  private indexOf( // unknown 和 any 差不多，只不过 unknow 声明的变量不能改
+    array: Array<(data?: unknown) => void>, 
+    item: (data?: unknown) => void
+  ): number {
     if (!array) return -1
     let index = -1
     for (let i = 0; i < array.length; i++) {
@@ -12,18 +17,18 @@ class EventHub {
     }
     return index
   }
-  on(eventName, fn) {
+  on(eventName: string, fn: (data?: unknown) => void): void {
     this.cache[eventName] = this.cache[eventName] || []
     this.cache[eventName].push(fn) // 监听注册过的事件
   }
 
   // 触发事件
-  emit(eventName, data?) {
-    (this.cache[eventName] || []).forEach(fn => fn(data)) // 执行注册过的事件
+  emit(eventName: string, data?: unknown): void {
+    (this.cache[eventName] || []).forEach((fn : (data?: unknown) => void) => fn(data)) // 执行注册过的事件
   }
 
   // 取消监听/注册事件
-  off(eventName, fn) {
+  off(eventName: string, fn: (data?: unknown) => void) {
     const index = this.indexOf(this.cache[eventName], fn)
     if (index === -1) return
     this.cache[eventName].splice(index, 1)
